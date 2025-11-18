66337ffa57545e6f804ba1b78a2593eceeeb3099
frontuser7 <aveer@sapat.com>
2025-11-18 13:01:53 +0530

    pydantic journey started

---
+++ basics.py
@@ -0,0 +1,16 @@
+from pydantic import BaseModel
+
+class User(BaseModel):
+    id:int
+    name: str
+    is_active: bool
+
+user_data = {
+    "id": 101,
+    "name" : "Ajinkya",
+    "is_active": True
+}
+
+user = User(**user_data)
+
+print(user)
